99d6bc31b08e79f105f632d3905d4b672689d00b
Sami Al-Jamal <sami.m.aljamal@gmail.com>
2016-06-10 14:26:33 -0700

    output will message empty input if user submits with filling the form.

--- js/scripts.js
+++ js/scripts.js
@@ -3,36 +3,32 @@ var indicator ='';
 var pingPong = function(number){
   for(var i= 1; i<= number; i++){
     if(i%3 == 0 && i%5 === 0){
-      indicator +="pingPong" + "<br>";
+      indicator += "<li>" + "pingPong" + "</li>";
+    } else if (i%3===0){
+      indicator += "<li>" + "ping" + "</li>";
+    } else if (i% 5 === 0){
+      indicator += "<li>" +"pong" + "</li>";
+    } else{
+      indicator += "<li>" + i + "</li>";
     }
-    else if (i%3===0){
-      indicator += "ping" + "<br>";
-
-    }else if (i% 5 === 0){
-      indicator += "pong" + "<br>";
-
-    }else{
-      indicator += i + "<br>";
-    }
-
-
-
-
   }
+
   return indicator;
-
-
 };
 
 // front-end logic
 $(document).ready(function(){
   $("form#pingpong").submit(function(event){
     event.preventDefault();
+    var complete = false;
     var number= $("input#number").val();
     var result = pingPong(number);
+    if (number === ""){
+      $("#answers").text("please enter a number").show();
 
-    $("#answers").append(result);
+    }else{$("#answers").append(result).show();}
+
+
 
   });
-
 });
