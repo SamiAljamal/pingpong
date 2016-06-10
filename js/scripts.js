4552fbf31090522f4e78c992ae40e4e4ca343bc2
Sami Al-Jamal <sami.m.aljamal@gmail.com>
2016-06-10 11:42:30 -0700

    output will list pingpong instead of the number if the number is divisible by 15.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,10 @@
 var indicator ='';
 var pingPong = function(number){
   for(var i= 1; i<= number; i++){
-    if (i%3===0){
+    if(i%3 == 0 && i%5 === 0){
+      indicator +="pingPong" + "<br>";
+    }
+    else if (i%3===0){
       indicator += "ping" + "<br>";
 
     }else if (i% 5 === 0){
