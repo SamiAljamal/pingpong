84bc1c4e7eb12b1790b6e5d3574dce0d3478a8d5
Sami Al-Jamal <sami.m.aljamal@gmail.com>
2016-06-10 11:25:18 -0700

    input will list the inputed number on the page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
 //business logic
+var indicator ='';
+var pingPong = function(number){
+  for(var i= 0; i<= number; i++){
+    indicator += i + "<br>";
+    
+
+
+  }
+  return indicator;
+
+
+};
 
 // front-end logic
 $(document).ready(function(){
   $("form#pingpong").submit(function(event){
     event.preventDefault();
     var number= $("input#number").val();
+    var result = pingPong(number);
 
-    $("#answers").text(number);
+    $("#answers").append(result);
 
   });
 
