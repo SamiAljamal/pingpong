8b895ad565738b0c80eb39b9e0d8a4a1c2cee3d3
Sami Al-Jamal <sami.m.aljamal@gmail.com>
2016-06-16 18:51:29 -0700

    fix the loop syntax, and add the Readme.Md file.

---
+++ README.md
@@ -0,0 +1,40 @@
+# PingPong
+
+#### Webpage that takes an entry and gives you an output according to certain conditions.
+
+#### By Sami Al-Jamal
+
+## Description
+The pingpong webpage works according to these specs.
+* the output will be a list of the the number the user inputs.
+  * input: 5
+  * output: 1,2,3,4,5
+* The output will return a different value in the list according to what that number is divisible by.
+  * if number is divisible by 3, output will equal "ping".
+    * input : 3
+    * output : 1,2,pin
+  * if number is divisible by 5, output will equal "pong".
+    * input : 5
+    * output : 1,2,ping,4,pong
+  * If number is divisible by 15, output will equal "pingpong"
+    * input : 5
+    * output : 1,2,ping,4,pong,ping,7,8,ping,pong,11,ping,13,14,pingpong
+
+
+## Known Bugs
+  Evertime the submit button is pressed, the output will append to the existing output.
+
+
+## Support and contact details
+For any concerns, please contact me at sami.m.aljamal@gmail.com
+## Technologies Used
+Technologies used in this program include:
+  * Html
+  * Css, bootstrap
+  * javascript
+  * Jquery
+
+### License
+This is licensed under the MIT license.
+
+Copyright (c) 2016 **Sami Al-Jamal**
--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,30 @@
 //business logic
-var indicator ='';
 var pingPong = function(number){
-  for(var i= 1; i<= number; i++){
-    if(i%3 == 0 && i%5 === 0){
-      indicator += "<li>" + "pingPong" + "</li>";
-    } else if (i%3===0){
-      indicator += "<li>" + "ping" + "</li>";
-    } else if (i% 5 === 0){
-      indicator += "<li>" +"pong" + "</li>";
-    } else{
-      indicator += "<li>" + i + "</li>";
+  var indicator = [];
+  for(var i=1; i<=number;i++){
+    if (i % 15 === 0){
+      indicator.push("pingpong");
+    } else if (i % 5 === 0){
+      indicator.push("pong");
+    } else if (i % 3 === 0){
+      indicator.push("ping");
+    // } else if (i % 5 === 0){
+    //   indicator.push("pong");
+    // }
+    } else {
+      indicator.push(i);
     }
   }
-return indicator;
+  return indicator;
 };
 // front-end logic
 $(document).ready(function(){
-  $("form#pingpong").submit(function(event){
+  $("form#pingpong").click(function(event){
     event.preventDefault();
-    var complete = false;
-    var number= $("input#number").val();
-    debugger;
-    var numPat= /^[0-9]*$/gm;
-    var tester= numPat.test(number);
+    var number= parseInt($("input#number").val());
     var result = pingPong(number);
-    if (number === ""){
-      $("#debug").text("please enter a number");
-      $(".error").show();
-      $(".answer").hide();
-    }else if (tester === false) {
-      $("#debug").text("please make sure you only enter numbers");
-      $(".error").show();
-      $(".answer").hide();
-    }else{
-      $("#answers").prepend(result);
-      $(".answer").show();
-      $(".error").hide()
-    }
-  });
-  $("button#refresh").click(function(){
-    $("#answers").empty();
-
+    result.forEach(function(result){
+      $("ul#answers").append("<li>" + result + "</li>");
+    });
   });
 });
